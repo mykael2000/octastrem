6a83bbdf8225a5ca2980fd270a113afd3179aed5
Michael Erameh <eramehmichael2000@gmail.com>
2024-04-28 08:06:33 +0100

    upload

---
+++ admin/includes/connection.php
@@ -0,0 +1,10 @@
+<?php
+
+$conn = mysqli_connect("s2950.fra1.stableserver.net", "Octastrem_octa", "oc234TaM12!", "Octastrem_octa");
+//$conn = mysqli_connect("localhost", "root", "", "Octastrem");
+
+// if($con){
+//     echo "connected";
+// }else{
+//     echo "failed";
+// }
---
+++ dash/user-area/includes/connection.php
@@ -0,0 +1,10 @@
+<?php
+
+$con = mysqli_connect("s2950.fra1.stableserver.net", "Octastrem_octa", "oc234TaM12!", "Octastrem_octa");
+//$con = mysqli_connect("localhost", "root", "", "Octastrem");
+
+// if($con){
+//     echo "connection successful";
+// }else{
+//     echo "connection failed";
+// }
