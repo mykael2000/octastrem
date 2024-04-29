e8645ebda62a7147825223ed140d471fad590ae1
Michael Erameh <eramehmichael2000@gmail.com>
2024-04-29 22:15:28 +0100

    upload

--- includes/header.php
+++ includes/header.php
@@ -62,8 +62,8 @@
                 <div class="site-navigation">
                     <div class="row g-0 align-items-center">
                         <div class="col-2">
-                            <a href="index.html" class="logo m-0 float-start"><img src="../octastremlogo.png"><span
-                                    class="text-primary"></span></a>
+                            <a href="index.html" class="logo m-0 float-start"><img src="../octastremlogo.png"
+                                    alt="logo"><span class="text-primary"></span></a>
                         </div>
                         <div class="col-6 text-center ">
                             <ul class="text-white js-clone-nav d-none d-lg-inline-block text-start site-menu mx-auto">
